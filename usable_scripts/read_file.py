da222c885ab1c89773809d3650f3139a4d90e93a
Vincent Joy <vincentjoyhere@gmail.com>
2024-09-13 20:08:55 +0530

    read scriptmexample

---
+++ usable_scripts/read_file.py
@@ -0,0 +1,24 @@
+import argparse
+import csv
+
+def handle_args():
+    """Handle arguments for scripts"""
+    parser = argparse.ArgumentParser(description='Sum values in csv')
+    parser.add_argument('path', help='Path to CSV file')
+    parser.add_argument('col', help='Name of column to sum values of')
+
+    return parser.parse_args()
+
+def main():
+    """Entry point to script"""
+    args = handle_args()
+    total = 0
+    with open(args.path) as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            total += int(row[args.col])
+
+    print(f'Total: {total}')
+
+if __name__ == '__main__':
+    main()
